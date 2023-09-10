960b80e82dcc98da2cf9815339e7efb34bed48e6
Rami B <ramibasheer9@gmail.com>
2023-09-10 07:42:47 +0200

    hover

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
-import React, { useState } from 'react';
-import SaudiArabiaMap from './components/SaudiArabiaMap';
-import CityModal from './components/CityModal';
+import React from "react";
+import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { saudiData } from "./data3";
+import "./App.css";
+
+const center = [23.713547526455798, 44.85715900050768];
 
 const App = () => {
-  const [selectedCity, setSelectedCity] = useState(null);
-
   return (
-    <div>
-      <SaudiArabiaMap />
-      <CityModal city={selectedCity} onHide={() => setSelectedCity(null)} />
+    <div className="container">
+      <h1>التوزيع الجغرافي للمشاركين في تقييم القدرات المؤسسية داخل المملكة</h1>
+      <MapContainer
+        center={center}
+        zoom={5.4}
+        style={{ height: "70vh", width: "70vw" }}
+        scrollWheelZoom={false}
+        touchZoom={false}
+        doubleClickZoom={false}
+        dragging={false}
+        zoomControl={false}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+
+        {saudiData.features.map((state) => {
+          const coordinates = state.geometry.coordinates[0].map((item) => [
+            item[1],
+            item[0],
+          ]);
+          return (
+            <Polygon
+              pathOptions={{
+                fillColor: "#FD8D3C",
+                color: "white",
+                fillOpacity: 0.7,
+                weight: 4,
+                opacity: 1,
+                dashArray: 3,
+              }}
+              positions={coordinates}
+              eventHandlers={{
+                mouseover: (e) => {
+               
+                  e.target
+                    .bindPopup(state.properties.name_arabic, {
+                      closeButton: false,
+                      maxWidth: "320px",
+                      minWidth: "320px",
+                      className: "popup",
+
+                      
+                    })
+                    .openPopup();
+                  const layer = e.target;
+                  layer.setStyle({
+                    weight: 4,
+                    color: "white",
+                    dashArray: "",
+                    fillOpacity: 0.7,
+                    fillColor: "#FACDCC",
+                   
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillColor: "#FD8D3C",
+                    color: "white",
+                    fillOpacity: 0.7,
+                    weight: 4,
+                    opacity: 1,
+                    dashArray: 3,
+                  });
+                },
+              }}
+            />
+          );
+        })}
+      </MapContainer>
     </div>
   );
 };
